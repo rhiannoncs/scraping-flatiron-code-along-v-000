b79f5c135e00e24e0d33ffdd120d3d43c76eaf96
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-02-01 00:25:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
     self.make_courses
     Course.all.each do |course|
       if course.title
-        puts "Title: #{course.title}"
+        puts "  Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
       end
